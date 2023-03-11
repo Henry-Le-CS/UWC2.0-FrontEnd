f0e1db92803352f0a3599c5646a718fd9954fcee
Khang Vo <nhatkhangdtp@gmail.com>
2023-03-11 16:54:47 +0700

    Help me with the nav bar

--- src/pages/BO/BO.js
+++ src/pages/BO/BO.js
@@ -1,10 +1,39 @@
-import React from "react";
-import "./Bo.css"
-export default function BO(){
+import React, { useState } from "react";
+import "./BO.css";
+
+export default function BO() {
+    const [showSidebar, setShowSidebar] = useState(false);
+
+    const toggleSidebar = () => {
+        setShowSidebar(!showSidebar);
+    };
+
     return (
-        <div>
-            <h1>BO site</h1>
-            <p>hi</p>
+        <div className="container" id="bo-container">
+            <button className="toggleSidebarBtn" onClick={toggleSidebar}>
+                {showSidebar ? "Hide" : "Show"} Sidebar
+            </button>
+            {console.log("BO", showSidebar)}
+            {showSidebar && (
+                <nav className="sidebar">
+                    <ul>
+                        <li>
+                            <a>Home</a>
+                        </li>
+                        <li>
+                            <a>About</a>
+                        </li>
+                        <li>
+                            <a>Contact</a>
+                        </li>
+                    </ul>
+                </nav>
+            )}
+
+            <main className="content">
+                <h1>BO site</h1>
+                <p>hi</p>
+            </main>
         </div>
-    )
+    );
 }
