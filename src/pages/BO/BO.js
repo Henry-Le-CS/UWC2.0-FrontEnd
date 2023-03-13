7227bf44ed4b46ed2ae0dd797fc030139260282a
Võ Hoàng Nhật Khang <nhatkhangdtp@gmail.com>
2023-03-13 11:36:31 +0700

    Update BO.js

--- src/pages/BO/BO.js
+++ src/pages/BO/BO.js
@@ -1,7 +1,5 @@
-
 import React, { useState } from "react";
 import "./BO.css";
-import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
 
 export default function BO() {
     const [showSidebar, setShowSidebar] = useState(false);
@@ -23,18 +21,35 @@ export default function BO() {
                                 <a href="#">
                                     <img src={require("../../assets/icon-sidebar/material-symbols_add-task.png")} alt="Add task" />
                                 </a>
-                                <a>Home</a>
+                                <a>Assign task</a>
                             </div>
                         </li>
                         <li>
-                            <a>About</a>
+                            <div class="assign-vehicles">
+                                <a href="#">
+                                    <img src={require("../../assets/icon-sidebar/material-symbols_fire-truck.png")} alt="Add vehicle" />
+                                </a>
+                                <a>Assign vehicles</a>
+                            </div>
                         </li>
                         <li>
-                            <a>Contact</a>
+                            <div class="assign-worker">
+                                <a href="#">
+                                    <img src={require("../../assets/icon-sidebar/mdi_worker.png")} alt="Add worker" />
+                                </a>
+                                <a>Assign workers</a>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="assign-MCP">
+                                <a href="#">
+                                    <img src={require("../../assets/icon-sidebar/basil_hotspot-solid.png")} alt="Add MCP" />
+                                </a>
+                                <a>View MCPs</a>
+                            </div>
                         </li>
                     </ul>
                 </nav>
-
             )}
 
             <main className="BO--content">
@@ -43,49 +58,4 @@ export default function BO() {
             </main>
         </div>
     );
-
-
-
-// function BO() {
-//   const { isLoaded } = useJsApiLoader({
-//     id: 'google-map-script',
-//     googleMapsApiKey: "AIzaSyAn7zRwQZC0MNs2kQIf8ATBBSW2ZzLXCtw"
-//   })
-
-//   const [map, setMap] = React.useState(null)
-
-//   const onLoad = React.useCallback(function callback(map) {
-//     // This is just an example of getting and using the map instance!!! don't just blindly copy!
-//     const bounds = new window.google.maps.LatLngBounds(center);
-//     map.fitBounds(bounds);
-
-//     setMap(map)
-//   }, [])
-
-//   const onUnmount = React.useCallback(function callback(map) {
-//     setMap(null)
-//   }, [])
-
-//   return isLoaded ? (
-//     <div>
-//       <h1>BO Site</h1>
-//       <div className="map">
-//         <GoogleMap
-//           mapContainerStyle={containerStyle}
-//           center={center}
-//           zoom={16}
-//           onLoad={onLoad}
-//           onUnmount={onUnmount}
-//         >
-//           <Marker
-//             position={center}
-//           />
-//           <></>
-//         </GoogleMap>
-//       </div>
-//     </div>
-//   ) : <></>
-// >>>>>>> main
-// }
-
-export default BO
+}
