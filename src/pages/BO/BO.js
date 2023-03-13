bc9cbb4f60e9e46374164940a95b43d496d7e1b7
Henry Le <henryle@191-16-11-208.user.vivozap.com.br>
2023-03-13 11:48:17 +0700

    style: move text to middle

--- src/pages/BO/BO.js
+++ src/pages/BO/BO.js
@@ -14,10 +14,10 @@ export default function BO() {
                 {showSidebar ? "Hide" : "Show"} Sidebar
             </button>
             {showSidebar && (
-                <nav class="BO--sidebar">
+                <nav className="BO--sidebar">
                     <ul>
                         <li>
-                            <div class="assign-task">
+                            <div className="assign-task">
                                 <a href="#">
                                     <img src={require("../../assets/icon-sidebar/material-symbols_add-task.png")} alt="Add task" />
                                 </a>
@@ -25,7 +25,7 @@ export default function BO() {
                             </div>
                         </li>
                         <li>
-                            <div class="assign-vehicles">
+                            <div className="assign-vehicles">
                                 <a href="#">
                                     <img src={require("../../assets/icon-sidebar/material-symbols_fire-truck.png")} alt="Add vehicle" />
                                 </a>
@@ -33,7 +33,7 @@ export default function BO() {
                             </div>
                         </li>
                         <li>
-                            <div class="assign-worker">
+                            <div className="assign-worker">
                                 <a href="#">
                                     <img src={require("../../assets/icon-sidebar/mdi_worker.png")} alt="Add worker" />
                                 </a>
@@ -41,7 +41,7 @@ export default function BO() {
                             </div>
                         </li>
                         <li>
-                            <div class="assign-MCP">
+                            <div className="assign-MCP">
                                 <a href="#">
                                     <img src={require("../../assets/icon-sidebar/basil_hotspot-solid.png")} alt="Add MCP" />
                                 </a>
